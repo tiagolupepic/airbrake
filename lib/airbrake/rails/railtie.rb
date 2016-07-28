2691f2692dd2091a86f155b1f1e13cfc28fd5744
Kyrylo Silin <silin@kyrylo.org>
2016-07-28 15:03:06 +0300

    rails/railtie: use `runner` API to handle exceptions in Rails runner

--- lib/airbrake/rails/railtie.rb
+++ lib/airbrake/rails/railtie.rb
@@ -68,6 +68,12 @@ module Airbrake
           include Airbrake::Rails::ActiveJob
         end
       end
+
+      runner do
+        at_exit do
+          Airbrake.notify_sync($!) if $!
+        end
+      end
     end
   end
 end
